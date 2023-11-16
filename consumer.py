b213e0b273ff0fafdd7d1f73edf9281c550bec84
Henry Denny <henry.denny12@gmail.com>
2023-11-16 14:49:11 +0000

    Compile schemas using `flatc` and deserialise detected messages.

--- consumer.py
+++ consumer.py
@@ -2,42 +2,64 @@ import flatbuffers
 from kafka import KafkaConsumer
 from kafka.consumer.fetcher import ConsumerRecord
 
-import DigitizerEventListMessage
+from DigitizerEventListMessage import *
 
 TRACE_TOPIC = "trace_in"
 
 
-def parse_msg(msg: ConsumerRecord) -> bytearray:
+def parse_msg(msg: ConsumerRecord) -> DigitizerEventListMessage:
     """
-    Parses messages detected on a particular trace_topic and serialises them using the flatbuffers schema.
+    Deserialises received messages using the flatbuffers schema.
 
     Parameters:
     msg (ConsumerRecord): The message to be parsed.
 
     Returns:
-    bytearray: The serialised buffer.
+    DigitizerEventListMessage: The deserialised payload.
     """
-    # Create a `FlatBufferBuilder`
-    builder = flatbuffers.Builder(1024)
-    DigitizerEventListMessage.Start(builder)
-    # DigitizerEventListMessage.AddVoltage(builder, voltage)
-    serialised = DigitizerEventListMessage.End(builder)
-    builder.Finish(serialised)
-    buf = builder.Output()
-    return buf
+    payload = DigitizerEventListMessage.GetRootAs(msg.value, 0)
+    return payload
+
+
+def display_payload_information(payload: DigitizerEventListMessage):
+    """
+    Displays the metadata and event list size information of a deserialised message.
+    """
+    metadata = payload.Metadata()
+    print("-------------------------------------------------------------")
+    print("[RECEIVED MESSAGE]:")
+    timestamp = metadata.Timestamp()
+    print(
+        "Timestamp:\t\t\t",
+        timestamp.Hour(),
+        ":",
+        timestamp.Minute(),
+        ", ",
+        timestamp.Second() * (10**9)
+        + timestamp.Millisecond() * (10**6)
+        + timestamp.Microsecond() * (10**3)
+        + timestamp.Nanosecond(),
+        " ns",
+    )
+    print("Running:\t\t\t", metadata.Running())
+    print("Veto Flags:\t\t\t", metadata.VetoFlags())
+    print("Frame Number:\t\t\t", metadata.FrameNumber())
+    print("Period Number:\t\t\t", metadata.PeriodNumber())
+    print("Protons Per Pulse:\t\t", metadata.ProtonsPerPulse())
+    print("-------------------------------------------------------------\n")
 
 
 def listen(trace_topic: str):
     """
-    Listens on a particular trace topic and attempts to pass any detected messages.
+    Listens on a particular trace topic and attempts to parse any detected messages.
 
     Parameters:
     trace_topic (str): The name of the trace topic.
     """
     consumer = KafkaConsumer(trace_topic)
     for msg in consumer:
-        buf = parse_msg(msg)
-        print(buf)
+        payload = parse_msg(msg)
+        display_payload_information(payload)
 
 
 listen(TRACE_TOPIC)
